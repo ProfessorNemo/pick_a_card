6b79579625bdebc739fd7a1dc527c47d78de5554
Gleb V. Zhegulin <gleboceanborn@gmail.com>
2022-05-06 11:42:37 +0300

    Alternative output & Joker

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -6,10 +6,13 @@ cards = []
 
 values.each do |value|
   suits.each do |suit|
-    cards << "#{value} of #{suit}"
+    cards << "[#{value}|#{suit}]"
   end
 end
 
+2.times do
+	cards << '[Joker]'
+end
 cards.shuffle!
 
 puts 'How many cards do you pick?'
